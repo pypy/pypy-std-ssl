91ecd9bd5999fef823489608cab8aa9787c4360c
Richard Plangger <planrichi@gmail.com>
2016-10-31 15:46:42 +0100

    set_curve_auto is a macro not a function

--- _cffi_src/openssl/ecdh.py
+++ _cffi_src/openssl/ecdh.py
@@ -12,20 +12,16 @@ INCLUDES = """
 
 TYPES = """
 static const int Cryptography_HAS_ECDH;
+static const int Cryptography_HAS_ECDH_SET_CURVE;
 """
 
 FUNCTIONS = """
-#if CRYPTOGRAPHY_OPENSSL_LESS_THAN_102 || defined(LIBRESSL_VERSION_NUMBER)
-static const long Cryptography_HAS_ECDH_SET_CURVE = 0;
-#else
-static const long Cryptography_HAS_ECDH_SET_CURVE = 1;
-int SSL_CTX_set_ecdh_auto(SSL_CTX *ctx, int onoff);
-#endif
 """
 
 MACROS = """
 int ECDH_compute_key(void *, size_t, const EC_POINT *, EC_KEY *,
                      void *(*)(const void *, size_t, void *, size_t *));
+int SSL_CTX_set_ecdh_auto(SSL_CTX *ctx, int onoff);
 """
 
 CUSTOMIZATIONS = """
@@ -38,5 +34,10 @@ int (*ECDH_compute_key)(void *, size_t, const EC_POINT *, EC_KEY *,
 
 #else
 static const long Cryptography_HAS_ECDH = 1;
+#endif
+#if CRYPTOGRAPHY_OPENSSL_LESS_THAN_102 || defined(LIBRESSL_VERSION_NUMBER)
+static const long Cryptography_HAS_ECDH_SET_CURVE = 0;
+#else
+static const long Cryptography_HAS_ECDH_SET_CURVE = 1;
 #endif
 """
